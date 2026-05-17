e58f0bbe56848abc52d18161b1be3a07eca84931
LamTranQueChi <lamtranquechi@gmail.com>
2026-05-17 17:26:14 +0700

    Add images

--- js/product-detail.js
+++ js/product-detail.js
@@ -10,9 +10,9 @@ const productList = [
 
     {
         id: 2,
-        name: "Hoa Tulip Hà Lan",
+        name: "Hoa Tulip hồng",
         price: "75000 / 5 củ",
-        image: "../assets/images/tulip.jpg",
+        image: "../assets/images/tuliphong.jpg",
         desc: "Tulip Hà Lan nổi bật với màu sắc thanh lịch và sang trọng. Loài hoa này thường nở vào mùa xuân, tạo nên khung cảnh nhẹ nhàng và tinh tế. Từ những khu vườn châu Âu nổi tiếng, Tulip đã trở thành biểu tượng của sự hạnh phúc và những khởi đầu tươi mới."
     },
 
